200f5049ebfd12a068eb104bcbf2d786530574a6
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-19 03:45:13 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,2 @@
 class ApplicationController < Sinatra::Base
   
-   configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
-  
-  get '/' do
-    redirect '/recipes'
-  end
-
-  get '/recipes' do
-    @recipes = Recipe.all
-    erb :index
-  end
-
-  get '/recipes/new' do
-    erb :new
-  end
-
-  
-end
